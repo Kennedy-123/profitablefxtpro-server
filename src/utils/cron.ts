7f1c010ae4e923c8fb94298e35f43b6d819c70c9
Kennedy-123 <okolochidera223@gmail.com>
2025-02-07 01:20:46 +0100

    modified

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -2,22 +2,20 @@ import cron from "node-cron";
 import User from "../models/User";
 
 export const startCronJobs = () => {
-  // Schedule the cron job to run every hour
+  // Schedule the cron job to run every 3 hours
   cron.schedule("0 */3 * * *", async () => {
     try {
-      // Check if any users have an amount greater than 0
-      const userDepositWallet = await User.countDocuments({ DepositWallet: { $gt: 0 } });
-      const userInterestWallet = await User.countDocuments({ interestWallet: { $gt: 0 } });
+      // Update users with interestWallet > 0 by incrementing the interestWallet and interestRate by 10
+      await User.updateMany(
+        { interestWallet: { $gt: 0 } },
+        { $inc: { interestWallet: 10, interestRate: 10 } }
+      );
 
-      if (userInterestWallet > 0) {
-        // Update all users by incrementing the interestWallet and interestRate by 0.1
-        await User.updateMany({}, { $inc: { interestWallet: 10, interestRate: 10 } });
-      }
-
-      if (userDepositWallet > 0) {
-        // Update all users by incrementing the DepositWallet and interestRate by 0.1
-        await User.updateMany({}, { $inc: { DepositWallet: 10, depositInterestRate: 10 } });
-      }
+      // Update users with DepositWallet > 0 by incrementing the DepositWallet and depositInterestRate by 10
+      await User.updateMany(
+        { DepositWallet: { $gt: 0 } },
+        { $inc: { DepositWallet: 10, depositInterestRate: 10 } }
+      );
     } catch (error) {
       console.error("Error in cron job:", error);
     }
